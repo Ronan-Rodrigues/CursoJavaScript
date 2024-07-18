db482c4787a2582804d2d6deeca4260ea62c99a7
Ronan-Rodrigues <ronansegpl22@hotmail.com>
2024-07-18 09:51:58 -0300

    Initial commit

---
+++ fundamentos.js
@@ -0,0 +1,31 @@
+//Comentários //
+
+//Fundamentos
+// var nome = "valor"
+var minhaVariavel = "Olá, Mundo";
+
+console.log(minhaVariavel)
+
+/*Comentário multi linhas
+Comentário
+Comentário
+*/
+
+//Variáveis e tipos de dados
+//numerico
+var meuNumero = 10;
+//texto
+var meuTexto = "Olá";
+
+//confere tipo de dados a variável está configurada
+console.log(typeof meuNumero);
+
+var booleano = true;
+
+console.log(typeof booleano);
+
+//variavel do tipo objeto
+var meuObjeto = {}
+
+
+
